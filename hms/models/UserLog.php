beac825ece8d054a2fb102fc4046118d4584bb4b
asega <asega@komputer-unyu>
2015-07-29 15:56:21 +0700

    Merge origin/master
    Conflicts:
    	js/controllers/t_keluar.js

--- hms/models/UserLog.php
+++ hms/models/UserLog.php
@@ -48,7 +48,7 @@ class UserLog extends CActiveRecord {
         // NOTE: you may need to adjust the relation name and the related
         // class name for the relations automatically generated below.
         return array(
-             'User' => array(self::HAS_MANY, 'User', 'id'),
+             'User' => array(self::BELONGS_TO, 'User', 'user_id'),
         );
     }
 
--- hms/views/user/auditUser.php
+++ hms/views/user/auditUser.php
@@ -6,25 +6,25 @@
         <th>Name User</th>
         <th>Time Log</th>
     </tr>
-    
-        <?php
-        $listUser = User::model()->listUser();
-        $oUserLogs = UserLog::model()->findAll(array('order' => 'created DESC','limit'=>50));
-        $no=0;
-        foreach ($oUserLogs as $oUserLog) {
-            $name = (isset($oUserLogs->User->name)) ? $oUserLogs->User->name :'-';
-            $roles = (isset($oUserLog->User->Roles->name)) ? $oUserLog->User->Roles->name :'Super User';
-            $no++;
-            if (isset($listUser[$oUserLog->user_id])) {
-               
-                echo'<tr>
-                    <td>'.$no.'</td>
-                    <td>'.$roles.'</td>
-                    <td>'.$oUserLog->User->name.'</td>
+
+    <?php
+    $listUser = User::model()->listUser();
+    $oUserLogs = UserLog::model()->findAll(array('order' => 'created DESC', 'limit' => 50));
+    $no = 0;
+    foreach ($oUserLogs as $oUserLog) {
+        $name = (isset($oUserLogs->User->name)) ? $oUserLogs->User->name : '-';
+        $roles = (isset($oUserLog->User->Roles->name)) ? $oUserLog->User->Roles->name : 'Super User';
+        $user = isset($oUserLog->User->name) ? $oUserLog->User->name : "-";
+        $no++;
+        if (isset($listUser[$oUserLog->user_id])) {
+            echo'<tr>
+                    <td>' . $no . '</td>
+                    <td>' . $roles . '</td>
+                    <td>' . $user . '</td>
                     <td><span class="label" style="margin-top: 6px;">' . landa()->ago($oUserLog->created) . '</span></td>
                         </tr>';
-            }
         }
-        ?>
-   
+    }
+    ?>
+
 </table>
