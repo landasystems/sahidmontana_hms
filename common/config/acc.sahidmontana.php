b0bc68b9291bc96dfc108b590b864f0bb4269272
asega <asega@komputer-unyu>
2015-07-29 10:44:07 +0700

    Merge origin/master
    Conflicts:
    	js/controllers/t_keluar.js

---
+++ common/config/acc.sahidmontana.php
@@ -0,0 +1,14 @@
+<?php
+//=========change this scope=========================================================
+$root = '/var/www/landa/sahidmontana_hms/';
+$themesUrl = 'http://localhost/landa/sahidmontana_hms/common/themes/';
+$db = 'landa_acc_sahidmontana';
+$db2 = 'landa_hms_sahidmontana';
+$dbUser = 'root';
+$dbPwd = 'bismillah';
+$client = 'hms.sahidmontana';
+$clientName = 'Accounting Hotel Sahid Montana';
+$menu = array('accounting');
+$rootUrl = 'http://localhost/landa/sahidmontana_hms/hms/www/'.$client.'/';
+//===================================================================================
+?>
