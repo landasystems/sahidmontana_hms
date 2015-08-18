5164fc917530a9ccf0a5a364846b323a32b6d6f8
x1n73ct <x1n73ct@x1n73ct>
2015-08-18 22:47:42 +0700

    Merge origin/master
    Conflicts:
    	tpl/blocks/header.html

--- hms/views/user/_search.php
+++ hms/views/user/_search.php
@@ -4,45 +4,19 @@
     </div>
 
     <div class="span4" style="padding-left: 90px;">
-        <div class="control-group">
-            <label class="control-label" for="inputEmail">Name</label>
-            <div class="controls">
-                <div class="input-append">
-                    <input class="span4" maxlength="255" name="User[name]" id="User_name" type="text">
-                    <button class="btn btn-primary" type="submit">Go!</button>
-                </div>
-            </div>
-        </div>
-
+        <?php echo $form->textFieldRow($model, 'email', array('class' => 'span4', 'maxlength' => 100)); ?>
     </div>
 </div>
 
 <div class="row">   
     <div class="span4">
         <?php echo $form->textFieldRow($model, 'code', array('class' => 'span4', 'maxlength' => 255)); ?>
-
-        <?php // echo $form->textFieldRow($model,'city_id',array('class'=>'span4')); ?>
-        <div class="control-group ">
-            <label>Province</label>
-            <div class="controls">
-                <?php
-                echo CHtml::dropDownList('province_id', 0, CHtml::listData(Province::model()->findAll(), 'id', 'name'), array(
-                    'empty' => 'Please Choose',
-                    'ajax' => array(
-                        'type' => 'POST',
-                        'url' => CController::createUrl('landa/city/dynacities'),
-                        'update' => '#User_city_id',
-                    ),
-                ));
-                ?>  
-            </div>
-        </div>
-        <?php echo $form->dropDownListRow($model, 'city_id', array(), array('class' => 'span3')); ?>
+        <?php echo $form->textFieldRow($model, 'name', array('class' => 'span4', 'maxlength' => 255)); ?>
     </div>
 
     <div class="span4" style="padding-left: 90px;">
 
-        <?php echo $form->textFieldRow($model, 'email', array('class' => 'span4', 'maxlength' => 100)); ?>
+
         <?php
         echo $form->textFieldRow(
                 $model, 'phone', array('prepend' => '+62')
